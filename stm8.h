53fe0afecdcd07e312c7b12cdcc7cff8078baa5e
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-01-16 14:52:53 +0200

    sync

---
+++ stm8.h
@@ -0,0 +1,10 @@
+#ifndef __stm8_h__
+#define __stm8_h__
+
+#if STM8_MCU == STM8S105K4
+#include <STM8S105K4.h>
+#else
+#error dont't know what is STM8_MCU
+#endif
+
+#endif
---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef __types_h__
+#define __types_h__
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define EDGE_LOW_LEVEL  0
+#define EDGE_RASING     1
+#define EDGE_FALLING    2
+#define EDGE_CHANGE     3
+
+#endif
